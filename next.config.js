ea6759e0113c4c43854ade9bbe8ed32d1746f033
MikeLee <liyangmike@gmail.com>
2025-05-18 23:02:42 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         pathname: '/**',
       },
